7ae292448cfead00bc65a974f0e51d469f22d703
GearBoxFox <98189655+GearBoxFox@users.noreply.github.com>
2022-02-18 11:00:57 -0500

    Create identify.py

---
+++ identify.py
@@ -0,0 +1,15 @@
+from PIL import Image
+import os.path
+import sys
+
+if len(sys.argv) > 1:
+    print(sys.argv[1])
+else:
+    sys.exit('Synta: identify.py [filename]')
+
+pic = sys.argv[1]
+img = Image.open(pic)
+x = img.size[0]
+y = img.size[1]
+
+print(x, y)
