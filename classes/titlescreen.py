daff330d1c876057d77e4bf589f7f2442e131ae5
GearBoxFox <98189655+GearBoxFox@users.noreply.github.com>
2023-07-12 12:33:18 -0400

    __init__.py

--- classes/titlescreen.py
+++ classes/titlescreen.py
@@ -1,4 +1,5 @@
 import pygame
+import Variables
 from pygame import draw, color, Rect, freetype
 
 freetype.init()
@@ -8,10 +9,18 @@ def run(window):
     startgame = False
     window.fill("purple")
 
+    startButton = Rect(Variables.worldx * 1/4, Variables.worldy * 1/2, Variables.worldx * 1/2, Variables.worldy * 1/4)
+
     while not startgame:
+        draw.rect(window, (0, 0, 255), startButton, 0, 1)
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                x = event.x
+                y = event.y
 
         pygame.display.flip()
 
