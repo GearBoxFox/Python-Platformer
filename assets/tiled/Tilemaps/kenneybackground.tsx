cef4d386761b2aaed279a6b2a710388b335fa309
Logan Hollowood <98189655+GearBoxFox@users.noreply.github.com>
2024-02-23 16:56:44 -0500

    imported kenney assets and tinkered with TiledMapEditor

---
+++ assets/tiled/Tilemaps/kenneybackground.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="kenneybackground" tilewidth="24" tileheight="24" spacing="1" margin="1" tilecount="14" columns="7">
+ <image source="../../Pixel Platformer/Tilemap/tilemap-backgrounds.png" width="199" height="74"/>
+</tileset>
